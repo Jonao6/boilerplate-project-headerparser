2896a90efa26360b4f84b53489ca775a2e16f7cb
Jonao6 <xeduardojonatas@gmail.com>
2023-06-01 17:03:16 +0000

    push: my github

--- index.js
+++ index.js
@@ -28,3 +28,13 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/whoami", (req, res) => {
+  const client = { 
+    "ipaddress": req.ip,
+   "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+                 }
+  res.json(client)
+  console.log(client)
+})
